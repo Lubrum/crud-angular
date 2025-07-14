7b21cdccab042a2104d8dde61a4f3ecea4e464cc
Lubrum <lucianobrum18@gmail.com>
2025-07-13 22:40:34 -0300

    Aula 22 Snackbar ações de erro ao salvar, httpclient post

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 import { SharedModule } from '../../shared/shared.module';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -15,21 +18,27 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackbar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
     })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-
+    console.log(this.form.value);
+    this.service.save(this.form.value).subscribe(result => console.log(result), error => { this.onError()});
   }
 
   onCancel() {
 
+  }
+
+  private onError() {
+    this.snackbar.open('Erro ao salvar curso.', '', { duration: 5000})
   }
 }

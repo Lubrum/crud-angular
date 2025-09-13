012bd386f1a26b518037a33e6e2cf4ad4aa68525
Lubrum <lucianobrum18@gmail.com>
2025-09-13 12:54:24 -0300

    Aula 56 e 57, validations e upgrade deps

--- crud-spring/src/main/java/com/brum/crud_spring/model/Course.java
+++ crud-spring/src/main/java/com/brum/crud_spring/model/Course.java
@@ -5,9 +5,10 @@ import com.brum.crud_spring.enums.Status;
 import com.brum.crud_spring.enums.converters.CategoryConverter;
 import com.brum.crud_spring.enums.converters.StatusConverter;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
@@ -15,7 +16,6 @@ import org.hibernate.validator.constraints.Length;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
 @SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
 @SQLRestriction("status = 'Ativo'")
@@ -41,7 +41,64 @@ public class Course {
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY, mappedBy = "course")
 //    @JoinColumn(name = "course_id")
     private List<Lesson> lessons = new ArrayList<>();
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", category=" + category +
+            ", status=" + status +
+            ", lessons=" + lessons +
+            '}';
+    }
 }
